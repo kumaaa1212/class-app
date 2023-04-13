7a32d408845941e343f27be1e3dbf60e37f3aad8
Ryohei <harrry.kuma1225@gmail.com>
2023-04-14 01:50:50 +0900

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,18 @@
 import { createContext, useEffect, useState } from 'react';
 import './App.css';
-import {useAuthState} from 'react-firebase-hooks/auth'
 import HomeArea from './component/HomeArea';
 import SettingArea from './component/SettingArea';
 import { TimetableData } from './countdata';
-import {  BrowserRouter, Link, Route, Routes } from 'react-router-dom'
-import Signup from './component/Signup';
-import { auth} from './firebase';
-import UserInfo from './component/UserInfo';
-import Signout from './component/Signout';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 export const Myclass = createContext();
 function App() {
   const [state, setstate] = useState(TimetableData);
-  useEffect(() =>{
+  useEffect(() => {
     if (window.localStorage) {
       const json = localStorage.getItem('key_name');
-        setstate(JSON.parse(json));
+      setstate(JSON.parse(json));
     }
-  },[])
-const [user] =useAuthState(auth);
+  }, []);
   return (
     <div className='conitaner'>
       <h1 className='maintitle'>Make the most of your time in college</h1>
@@ -27,27 +21,15 @@ const [user] =useAuthState(auth);
           <ul className='linklist'>
             <Link to='/'><li>HOME</li></Link>
             <Link to='/SettingArea'><li>設定</li></Link>
-            { user ? (
-              <>
-              <UserInfo/>
-              <Signout/>
-              </>
-            ):(
-              <Signup />
-            )}
-            </ul>
+          </ul>
         </div>
         <div>
-          { user ? (
-               <Myclass.Provider value={[state, setstate]}>
-               <Routes>
-                 <Route path='/' element={<HomeArea />} />
-                 <Route path='/SettingArea' element={<SettingArea />} />
-               </Routes>
-             </Myclass.Provider>
-          ) : (
-            <h1>ログインしてください</h1>
-          )}
+          <Myclass.Provider value={[state, setstate]}>
+            <Routes>
+              <Route path='/' element={<HomeArea />} />
+              <Route path='/SettingArea' element={<SettingArea />} />
+            </Routes>
+          </Myclass.Provider>
         </div>
       </BrowserRouter>
     </div>
