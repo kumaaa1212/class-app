5bbc161d6139d4eaf9cf807d0a9e693d6634b800
Ryohei <harrry.kuma1225@gmail.com>
2023-04-16 01:19:40 +0900

    commit

--- my-app/src/component/CaveatArea.js
+++ my-app/src/component/CaveatArea.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import '.././style/caution.css'
-const CaveatArea = ({cautoinstate,notionstate}) => {
-  const showcautionclass =Array.from(new Set(cautoinstate));
-  const  shownotionclass=Array.from(new Set(notionstate));
+const CaveatArea = ({ cautoinstate, notionstate }) => {
+  const showcautionclass = Array.from(new Set(cautoinstate)) || [];
+  const shownotionclass = Array.from(new Set(notionstate)) || [];
   return (
     <div className='container'>
       <div>
         <h1>取得不可講義</h1>
         <ul>
-        {showcautionclass.map((list) =>(
-          <li key={list}>{list}</li>
-        ))}
+          {showcautionclass.map((list) => (
+            <li key={list}>{list}</li>
+          ))}
         </ul>
       </div>
       <div>
         <h1>注意講義</h1>
         <ul>
-        {shownotionclass.map((list) =>(
-          <li key={list}>{list}</li>
-        ))}
+          {shownotionclass.map((list) => (
+            <li key={list}>{list}</li>
+          ))}
         </ul>
       </div>
     </div>
   )
 }
 
-export default CaveatArea 
+export default CaveatArea;
